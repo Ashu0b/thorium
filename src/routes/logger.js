48216f751aebe242b65391d1e8712af045b7446c
Ashis-Thorium <ashisbisoi05@gmail.com>
2022-02-22 05:07:44 +0530

    i changed the message

---
+++ src/routes/logger.js
@@ -0,0 +1,6 @@
+let url = "https://www.google.com/"
+function Welcome (message){
+    console.log(message)
+}
+module.exports.endpoint=url
+module.exports.printMessage=Welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+let obj=require('./logger')
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+     res.send('Welcome to my application. I am Ashis Bisoi and a part of FunctionUp Thorium cohort')
+     console.log(obj.endpoint)
+    obj.Welcome('Welcome')
+   
 });
 
 module.exports = router;
