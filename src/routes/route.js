ad3209864a7eee8e4fb95d4dc74207ae3bedcb14
Ashis-Thorium <ashisbisoi05@gmail.com>
2022-02-22 22:14:44 +0530

    i just changed all

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 let obj=require('./logger')
 router.get('/test-me', function (req, res) {
-     res.send('Welcome to my application. I am Ashis Bisoi and a part of FunctionUp Thorium cohort')
+     res.send('Welcome to my application. I am Ashis and a part of FunctionUp Thorium cohort')
      console.log(obj.endpoint)
-    obj.Welcome('Welcome')
+    obj.log('Welcome')
    
 });
 
