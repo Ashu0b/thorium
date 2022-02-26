616ed9064394170a54635a966d5fe4ad7ab65791
Ashis-Thorium <ashisbisoi05@gmail.com>
2022-02-26 20:38:01 +0530

    practice solution

--- src/logger/logger.js
+++
@@ -1,8 +0,0 @@
-// module 1
-let obj=require('./logger')
-router.get('/test-me', function (req, res) {
-     res.send('Welcome to my application. I am Ashis and a part of FunctionUp Thorium cohort')
-     console.log(obj.endpoint)
-    obj.log('Welcome')
-   
-});
